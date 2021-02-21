621b94153be85c18da9d670bcda87061ab2fdb8b
ArshikAkash <arshikakash@gmail.com>
2021-02-21 19:40:44 +0600

    Alhamdulillah Done

--- app.js
+++ app.js
@@ -23,10 +23,12 @@ const showImages = (images) => {
 		div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
 		div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
 		gallery.appendChild(div);
+		toggleSpinner(false);
 	});
 };
 
 const getImages = (query) => {
+	toggleSpinner(true);
 	fetch(
 		`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`
 	)
@@ -132,3 +134,12 @@ searchBtn.addEventListener('click', function () {
 sliderBtn.addEventListener('click', function () {
 	createSlider();
 });
+
+const toggleSpinner = (show) => {
+	const spinner = document.getElementById('loading-spinner');
+	if (show === true) {
+		spinner.classList.remove('d-none');
+	} else {
+		spinner.classList.add('d-none');
+	}
+};
